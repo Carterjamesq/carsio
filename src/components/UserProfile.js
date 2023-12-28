125abf408f60064a6f2bbdf2473e6ab839a6ccec
Vitalii Rabchebskyi <carterjames@ukr.net>
2023-12-28 14:52:05 +0200

    Adding heading

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const UserProfile = () => {
   return (
     <div>
       <p>User Profile</p>
+      <h2>Heading h2</h2>
     </div>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
