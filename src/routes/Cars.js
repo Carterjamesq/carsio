d38892ff95ce7aafb516aca233bf2ea340692274
Vitalii Rabchebskyi <carterjames@ukr.net>
2024-01-09 21:47:02 +0200

    Added Filtering

--- src/routes/Cars.js
+++ src/routes/Cars.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 import { fetchContent } from "../dataset/carsDB";
@@ -9,17 +9,73 @@ import { IoColorFillOutline } from "react-icons/io5";
 
 const Cars = () => {
   const [content, setContent] = useState([]);
+  const [sortingOption, setSortingOption] = useState("Please select sort"); // Set default sorting option
+  const [selectedFilter, setSelectedFilter] = useState("");
+  const initialContentRef = useRef([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchContent();
       setContent(data);
+      initialContentRef.current = data;
     };
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    // Apply filter logic here if needed based on selectedFilter
+    const sortedArray = sortContent(initialContentRef.current);
+    setContent(sortedArray);
+  }, [sortingOption, selectedFilter]);
+
+  const sortContent = (data) => {
+    switch (sortingOption) {
+      case "A-Z":
+        return [
+          ...data.sort((a, b) => a.fields.make.localeCompare(b.fields.make)),
+        ];
+
+      case "Newest":
+        return [
+          ...data.sort(
+            (a, b) => new Date(b.fields.year) - new Date(a.fields.year)
+          ),
+        ];
+      case "Oldest":
+        return [
+          ...data.sort(
+            (a, b) => new Date(a.fields.year) - new Date(b.fields.year)
+          ),
+        ];
+      case "PriceHighToLow":
+        return [...data.sort((a, b) => b.fields.price - a.fields.price)];
+      case "PriceLowToHigh":
+        return [...data.sort((a, b) => a.fields.price - b.fields.price)];
+      default:
+        return data;
+    }
+  };
+
+  const handleSortChange = (event) => {
+    setSortingOption(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setSelectedFilter(event.target.value);
+  };
   return (
     <div>
+      <div className="filter-form">
+        <select value={sortingOption} onChange={handleSortChange}>
+          <option value="">Please Select Sorting</option>
+          <option value="A-Z">A-Z</option>
+          <option value="Newest">Newest</option>
+          <option value="Oldest">Oldest</option>
+          <option value="PriceHighToLow">Price High to Low</option>
+          <option value="PriceLowToHigh">Price Low to High</option>
+        </select>
+      </div>
       <div className="cars">
         <ul className="cars-cards">
           {content.map((item) => (
@@ -43,7 +99,7 @@ const Cars = () => {
                   </div>
                   <div className="card-flex">
                     <PiEngineBold />
-                    <p>{item.fields.fuel_type}</p>
+                    <p>{item.fields.fuelType}</p>
                   </div>
                   <div className="card-flex">
                     <IoColorFillOutline />
